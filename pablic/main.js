76e16778e052fb35252a55c2b082edb1dcecb591
Dima1981 <dimyhametit@gmail.com>
2018-01-10 22:58:28 +0300

    map

---
+++ pablic/main.js
@@ -0,0 +1,34 @@
+$(".msk").click(function () {
+			$(".map__metka1").animate(
+			{
+			opacity: 1
+			})
+		});
+
+$(".kzn").click(function () {
+			$(".map__metka2").animate(
+			{
+			opacity: 1
+			})
+		});
+
+$(".ek").click(function () {
+			$(".map__metka3").animate(
+			{
+			opacity: 1
+			})
+		});
+
+$(".cam").click(function () {
+			$(".map__metka4").animate(
+			{
+			opacity: 1
+			})
+		});
+
+$(".orwn").click(function () {
+			$(".map__metka5").animate(
+			{
+			opacity: 1
+			})
+		});
